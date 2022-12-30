d245aa82dd3bca196bce98f7d8380a972d13d7b7
matheus lisboa <matheus.lisboa.santos@ccc.ufcg.edu.br>
2022-12-30 20:36:29 -0300

    adding more core

--- rest-with-spring-boot/src/main/java/br/com/wineone/controllers/PersonController.java
+++ rest-with-spring-boot/src/main/java/br/com/wineone/controllers/PersonController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -80,9 +82,13 @@ public class PersonController {
 	)
 	public ResponseEntity<Page<PersonVOResponse>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "limit", defaultValue = "12") Integer limit
+			@RequestParam(value = "limit", defaultValue = "12") Integer limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction
 			) throws ResourceNotFoundException {
-		Pageable pageable = PageRequest.of(page, limit);
+		
+		var sortDirection = "asc".equalsIgnoreCase(direction) ? Direction.ASC : Direction.DESC;
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection,"firstName"));
+		
 		return ResponseEntity.ok(personServices.findAll(pageable));
 	}
 	
